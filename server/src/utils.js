d38c6c740c0191b5c2fdcf7b046de09dfc00d1c2
austinb12 <austinzbaird@gmail.com>
2025-11-24 17:03:10 -0500

    locale

--- server/src/utils.js
+++ server/src/utils.js
@@ -2,6 +2,6 @@ import dayjs from 'dayjs';
 
 export function format_sql_datetime(date) {
   const date_obj = typeof date === 'string' ? new Date(date) : date;
-  const js_date = dayjs(date_obj);
+  const js_date = dayjs(date_obj.toLocaleString());
   return js_date.format('YYYY-MM-DD HH:mm:ss');
 }
